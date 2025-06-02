5149105f7c6a4e77af923ed1b89c4190a73cfb3f
Alexey Moltyanskiy <alexmolt@mail.ru>
2025-06-02 18:39:35 +0300

    Add files via upload

--- 19/script.js
+++ 19/script.js
@@ -1,16 +1,75 @@
+// global variables
+
 const persons = [];
 const personId = document.getElementById('personId');
 const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
-const age = document.getElementById('age');
+const birthdate = document.getElementById('birthdate');
 const addPerson = document.getElementById('addPerson');
 const personList = document.getElementById('personList');
 const stats = document.getElementById('stats');
+const today = new Date();
+
+// constructors
+
+function newDOMElement(element, parent, text = "", className = "") {
+    const elem = document.createElement(element);
+    if (text) elem.innerText = text;
+    parent.appendChild(elem);
+    if (className) elem.classList.add(className);
+    return elem;
+}
+
+function Person(id, firstName, lastName, birthdate) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthdate = new Date(birthdate);
+    this.age = () => {
+        const yearToday = today.getFullYear();
+        const monthToday = today.getMonth();
+        const dayToday = today.getDate();
+
+        const yearBirth = this.birthdate.getFullYear();
+        const monthBirth = this.birthdate.getMonth();
+        const dayBirth = this.birthdate.getDate();
+
+        const age = yearToday - yearBirth;
+
+        if (monthBirth < monthToday) {
+            return age;
+        }
+        if (monthBirth === monthBirth) {
+            if (dayBirth <= dayToday) {
+                return age;
+            } else return age - 1;
+        }
+        if (monthBirth > monthToday) {
+            return age - 1;
+        }
+    }
+}
+
+// input operations
+
+
+const maxDate = new Date(today); // копируем, чтобы не трогать оригинальный today
+birthdate.max = maxDate.toISOString().split('T')[0]; // Устанавливаем границы
+
+
+function birthdateInputNotCorrect(birthdateValue) {
+    const enteredDate = new Date(birthdateValue);
+    const minDate = new Date();
+    minDate.setFullYear(1900);
+    return (birthdateValue === "" ||
+        isNaN(enteredDate.getTime()) ||
+        enteredDate > today ||
+        enteredDate < minDate);
+}
 
 function checkInputs() {
     if (isNaN(personId.value) || personId.value === "") {
         customAlert('Please enter a valid ID (Only numbers)')
-        // alert('Please enter a valid ID (Only numbers)');
         personId.focus();
         return false;
     }
@@ -24,26 +83,28 @@ function checkInputs() {
         lastName.focus();
         return false
     }
-    if (isNaN(personId.value) || age.value === "" || age.value > 150 || age.value < 0) {
-        customAlert('Please enter a valid age');
-        age.focus();
+    if (birthdateInputNotCorrect(birthdate.value)) {
+        customAlert('Please enter a valid birthdate');
+        birthdate.focus();
         return false;
     }
     return true;
 }
 
-function newElement(element, parent, text = "", className = "") {
-    const elem = document.createElement(element);
-    if (text) elem.innerText = text;
-    parent.appendChild(elem);
-    if (className) elem.classList.add(className);
-    return elem;
+function clearInputs() {
+    personId.value = '';
+    firstName.value = '';
+    lastName.value = '';
+    birthdate.value = '';
+    personId.focus();
 }
+
+// main logic
 
 function createPerson() {
     if (!checkInputs()) return;
     if (persons.findIndex(person => person.id === personId.value) === -1) {
-        let newPerson = new Person(personId.value, firstName.value, lastName.value, age.value);
+        let newPerson = new Person(personId.value, firstName.value, lastName.value, birthdate.value);
         persons.push(newPerson);
         printList();
         statsUpdate();
@@ -52,69 +113,65 @@ function createPerson() {
     personId.focus()
 }
 
-function clearInputs() {
-    personId.value = '';
-    firstName.value = '';
-    lastName.value = '';
-    age.value = '';
-    personId.focus();
-}
-
 function printList() {
     personList.innerText = '';
     persons.forEach((person) => {
-        const personRow = newElement('div', personList, '', 'personRow')
+        const personRow = newDOMElement('div', personList, '', 'personRow')
         personInDiv(personRow, person)
         personList.appendChild(personRow);
-        const divButton = newElement('div', personRow, '', 'cell');
+        const divButton = newDOMElement('div', personRow, '', 'cell');
         buttonEdit(person, personRow, divButton);
         buttonDelete(person, personRow, divButton);
     })
-}
-
-function personInDiv(personRow, person) {
-    newElement('div', personRow, `${person.firstName}`, 'cell');
-    newElement('div', personRow, `${person.lastName}`, 'cell');
-    newElement('div', personRow, `${person.age}`, 'cell');
-    newElement('div', personRow, `${person.id}`, 'cell');
-}
-
-function buttonDelete(currentPerson, divPerson, divButton) {
-    const trash = newElement('img', divButton, '', 'imgTrash');
-    trash.onclick = () => {
-        if (confirm('Are you sure you want to delete this person?')) {
-            let indexCurrentPerson = persons.findIndex(person => person.id === currentPerson.id);
-            persons.splice(indexCurrentPerson, 1);
-            divPerson.remove();
-            statsUpdate();
-        }
-    }
-}
-
-function buttonEdit(currentPerson, personRow, divButton) {
-    const edit = newElement('img', divButton, '', 'imgEdit');
-    edit.onclick = () => {
-        //TODO...
-    }
 }
 
 function statsUpdate() {
     stats.innerText = '';
     if (persons.length > 0) {
         // total persons
-        newElement("p", stats, `Total persons in company: ${persons.length}`);
+        newDOMElement("p", stats, `Total persons in company: ${persons.length}`);
         // min age
-        const minAge = Math.min(...persons.map(person => person.age))
-        newElement("p", stats, `Minimum age: ${minAge}`);
+        const minAge = Math.min(...persons.map(person => person.age()))
+        newDOMElement("p", stats, `Minimum age: ${minAge}`);
         // max age
-        const maxAge = Math.max(...persons.map(person => person.age));
-        newElement("p", stats, `Maximum age: ${maxAge}`);
+        const maxAge = Math.max(...persons.map(person => person.age()));
+        newDOMElement("p", stats, `Maximum age: ${maxAge}`);
         // average age
-        const ageSum = persons.reduce((sum, person) => sum + person.age, 0);
+        const ageSum = persons.reduce((sum, person) => sum + person.age(), 0);
         const averageSum = ageSum / persons.length;
-        newElement("p", stats, `Average age: ${Math.round(averageSum)}`);
-    } else newElement('p', stats, 'Add person to get statistics');
+        newDOMElement("p", stats, `Average age: ${Math.round(averageSum)}`);
+    } else newDOMElement('p', stats, 'Add person to get statistics');
 }
+
+function personInDiv(parent, person) {
+    newDOMElement('div', parent, `${person.firstName}`, 'cell');
+    newDOMElement('div', parent, `${person.lastName}`, 'cell');
+    newDOMElement('div', parent, `${person.age()}`, 'cell');
+    newDOMElement('div', parent, `${person.id}`, 'cell');
+}
+
+// buttons
+
+function buttonEdit(currentPerson, personRow, divButton) {
+    const edit = newDOMElement('img', divButton, '', 'imgEdit');
+    edit.onclick = () => {
+        //TODO...
+    }
+}
+
+function buttonDelete(currentPerson, personRow, divButton) {
+    const trash = newDOMElement('img', divButton, '', 'imgTrash');
+    trash.onclick = () => {
+        if (confirm('Are you sure you want to delete this person?')) {
+            let indexCurrentPerson = persons.findIndex(person => person.id === currentPerson.id);
+            persons.splice(indexCurrentPerson, 1);
+            personRow.remove();
+            statsUpdate();
+        }
+    }
+}
+
+// alerts
 
 function customAlert(text) {
     const alert = document.createElement('div');
@@ -131,14 +188,7 @@ function customAlert(text) {
     }, 3000);
 }
 
-function Person(id, firstName, lastName, age) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = Number(age);
-}
-
-// listeners
+// event listeners
 
 addPerson.onclick = function () {
     createPerson();
